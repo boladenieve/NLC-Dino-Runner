67541e7108d0316a52f39082a0f085fe08cfb10b
Miguel Alarcon <rossmell123987@gmail.com>
2021-09-09 19:51:56 -0400

    Clase 3: Dino

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,6 +1,7 @@
 import pygame
-
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, BG, FPS
+from nlc_dino_runner.components.dinosaur import Dinosaur
+
 
 
 class Game:
@@ -9,12 +10,12 @@ class Game:
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
         self.clock = pygame.time.Clock()
-        self.scream = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.playing = False
-        self.game_speed = 10
+        self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 360
-
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -30,21 +31,21 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.scream.fill((255,255,255))
+        self.screen.fill((255,255,255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width()
-        self.scream.blit(BG, (self.x_pos_bg, self.y_pos_bg))
-
-        self.scream.blit(BG, (self.x_pos_bg + image_width, self.y_pos_bg))
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (self.x_pos_bg + image_width, self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
-            self.scream.blit(BG, (self.x_pos_bg + image_width, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
